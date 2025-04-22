136215993532720aff1bf3b23758f17aedeeae08
Michael Bestas <mkbestas@lineageos.org>
2025-04-22 13:17:23 +0300

    thermal: max31760_fan: Import nicole changes
    Based on nicole msm-5.4.254.tar.bz2.
    
    Change-Id: Ie0bdbfec6844ba86c3c006d5e716c4a57dd3cc7d

--- drivers/thermal/qcom/max31760_fan.c
+++ drivers/thermal/qcom/max31760_fan.c
@@ -9,21 +9,53 @@
 #include <linux/of_gpio.h>
 #include <linux/regulator/consumer.h>
 #include <linux/thermal.h>
+#ifdef CONFIG_MACH_RAZER_NICOLE
+#include <linux/time.h>
+#include <linux/timer.h>
+#endif
 
 #define MAX31760_CTRL_REG1		0x00
 #define MAX31760_CTRL_REG2		0x01
 #define MAX31760_CTRL_REG3		0x02
 #define MAX31760_DUTY_CYCLE_CTRL_REG	0x50
+#ifdef CONFIG_MACH_RAZER_NICOLE
+#define MAX31760_DUTY_CYCLE_STAT_REG	0x51
+#define MAX31760_TC1H			0x52
+#define MAX31760_TC1L			0x53
+#endif
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+#define VDD_MAX_UV	3300000
+#define VDD_MIN_UV	3300000
+#define VDD_MAX_FAN	2900000
+#define VDD_MIN_FAN	2800000
+#else
 #define VDD_MAX_UV	3100000
 #define VDD_MIN_UV	3000000
+#endif
 #define VDD_LOAD_UA	300000
 #define VCCA_MAX_UV	1800000
 #define VCCA_MIN_UV	1800000
 #define VCCA_LOAD_UA	600000
 #define FAN_SPEED_LEVEL0	0
+#ifdef CONFIG_MACH_RAZER_NICOLE
+#define FAN_SPEED_LEVEL7	7
+#define FAN_SPEED_MAX		(FAN_SPEED_LEVEL7 + 1)
+#define FAN_SPEED_DUTY_MAX	100
+#define FAN_SPEED_DUTY_ARRY_MAX	(FAN_SPEED_DUTY_MAX + 1)
+#define FAN_SPEED_RPM_MAX	18000
+#define FAN_SPEED_ERROR		3600
+#define FAN_SPEED_RPM_MIN	0
+
+#define FAN_SPEED_RPM_ERROR_LEVEL_0	50
+#define FAN_SPEED_RPM_ERROR_LEVEL_1	150
+#define FAN_SPEED_RPM_ERROR_LEVEL_2	350
+#define FAN_SPEED_RPM_ERROR_LEVEL_3	680
+#define FAN_SPEED_RPM_SAMPLE_TIMES	1
+#else
 #define FAN_SPEED_LEVEL4	4
 #define FAN_SPEED_MAX		5
+#endif
 
 struct max31760_data {
 	struct device *dev;
@@ -32,12 +64,23 @@ struct max31760_data {
 	struct mutex update_lock;
 	struct regulator *vdd_reg;
 	struct regulator *vcca_reg;
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	struct regulator *vdda_reg;
+	u32 dpr_en_gpio;
+	u32 usb_charge_en_gpio;
+#else
 	u32 max31760_en_gpio;
+#endif
 	unsigned int cur_state;
 	atomic_t in_suspend;
 };
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+struct max31760_data *Max31760_pdata = NULL;
+static int max31760_speed_map[FAN_SPEED_MAX] = {0x00, 0x4e, 0x71, 0x97, 0xb2, 0xcc, 0xe8, 0xFF};
+#else
 static int max31760_speed_map[FAN_SPEED_MAX] = {0x00, 0x30, 0x85, 0xCF, 0xFF};
+#endif
 
 static int max31760_write_byte(struct max31760_data *pdata, u8 reg, u8 val)
 {
@@ -54,23 +97,183 @@ static int max31760_write_byte(struct max31760_data *pdata, u8 reg, u8 val)
 	return 0;
 }
 
+#ifndef CONFIG_MACH_RAZER_NICOLE
 static void max31760_enable_gpio(struct max31760_data *pdata, int on)
 {
 	gpio_direction_output(pdata->max31760_en_gpio, on);
 	dev_dbg(pdata->dev, "gpio:%d set to %d\n", pdata->max31760_en_gpio, on);
 	usleep_range(20000, 20100);
 }
+#endif
 
 static void max31760_speed_control(struct max31760_data *pdata, unsigned long level)
 {
 	max31760_write_byte(pdata, MAX31760_DUTY_CYCLE_CTRL_REG, max31760_speed_map[level]);
 }
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+static int max31760_read_reg(unsigned char reg)
+{
+	struct max31760_data *pdata = Max31760_pdata;
+	struct i2c_client *client = pdata->i2c_client;
+	int value = -1;
+
+	value = i2c_smbus_read_byte_data(client, reg);
+	if (value < 0) {
+		dev_err(pdata->dev, "failed read reg %x: %x\n", reg, value);
+	}
+
+	return value;
+}
+
+static int get_fan_current_speed_rpm(void)
+{
+	int tc1h = 0, tc1l = 0;
+	int value = 0;
+	struct max31760_data *pdata = Max31760_pdata;
+	struct i2c_client *client = pdata->i2c_client;
+
+	tc1h = i2c_smbus_read_byte_data(client, MAX31760_TC1H);
+	if (tc1h < 0) {
+		dev_err(pdata->dev, "failed read tc1h %d %#x\n", tc1h, tc1h);
+		value = -1;
+	}
+
+	tc1l = i2c_smbus_read_byte_data(client, MAX31760_TC1L);
+	if (tc1l < 0) {
+		dev_err(pdata->dev, "failed read tc1l %d %#x\n", tc1l, tc1l);
+		value = -2;
+	}
+
+	value = 60 * 100000 / (tc1h * 256 + tc1l) / 2;
+
+	return value;
+}
+
+static ssize_t fan_speed_store(struct device *dev,
+			       struct device_attribute *attr,
+			       const char *buf, size_t count)
+{
+	int level;
+
+	if (sscanf(buf, "%d", &level) != 1)
+		return count;
+	if (level < 0 || level > 255) {
+		printk("max31760_fan_speed_set_by_duty parameter error, level=%d, must in 0 to 100\n",
+				level);
+		return count;
+	}
+
+	mutex_lock(&Max31760_pdata->update_lock);
+        max31760_write_byte(Max31760_pdata, MAX31760_DUTY_CYCLE_CTRL_REG,
+			(u8)level);
+	mutex_unlock(&Max31760_pdata->update_lock);
+
+	return count;
+}
+
+static ssize_t fan_speed_show(struct device *dev,
+			      struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", (int)get_fan_current_speed_rpm());
+}
+
+static ssize_t fan_duty_show(struct device *dev,
+			     struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n",
+			max31760_read_reg(MAX31760_DUTY_CYCLE_STAT_REG));
+}
+
+static ssize_t usb_charge_en_store(struct device *dev,
+				   struct device_attribute *attr,
+				   const char *buf, size_t count)
+{
+	int status;
+
+	if (sscanf(buf, "%d", &status) != 1)
+		return count;
+
+	if (status <= 0) {
+		status = 0;
+	} else if (status > 0) {
+		status = 1;
+	}
+
+	mutex_lock(&Max31760_pdata->update_lock);
+	if (gpio_is_valid(Max31760_pdata->usb_charge_en_gpio)) {
+		gpio_direction_output(Max31760_pdata->usb_charge_en_gpio,
+				status);
+	}
+	mutex_unlock(&Max31760_pdata->update_lock);
+
+	return count;
+}
+
+static ssize_t usb_charge_en_show(struct device *dev,
+				  struct device_attribute *attr, char *buf)
+{
+	int status = -1;
+
+	if (gpio_is_valid(Max31760_pdata->usb_charge_en_gpio)) {
+		status = gpio_get_value(Max31760_pdata->usb_charge_en_gpio);
+	}
+
+	return sprintf(buf, "%d\n",status);
+}
+
+static ssize_t dpr_en_store(struct device *dev,
+			    struct device_attribute *attr,
+			    const char *buf, size_t count)
+{
+	int status;
+
+	if (sscanf(buf, "%d", &status) != 1)
+		return count;
+
+	if (status <= 0) {
+		status = 0;
+	} else if (status > 0) {
+		status = 1;
+	}
+
+	mutex_lock(&Max31760_pdata->update_lock);
+	if (gpio_is_valid(Max31760_pdata->dpr_en_gpio)) {
+		gpio_direction_output(Max31760_pdata->dpr_en_gpio, status);
+	}
+	mutex_unlock(&Max31760_pdata->update_lock);
+
+	return count;
+}
+
+static ssize_t dpr_en_show(struct device *dev,
+			   struct device_attribute *attr, char *buf)
+{
+	int status = -1;
+
+	if (gpio_is_valid(Max31760_pdata->dpr_en_gpio)) {
+		status = gpio_get_value(Max31760_pdata->dpr_en_gpio);
+	}
+
+	return sprintf(buf, "%d\n",status);
+}
+
+static DEVICE_ATTR(fan_speed, 0664, fan_speed_show, fan_speed_store);
+static DEVICE_ATTR(usb_charge_en, 0664, usb_charge_en_show, usb_charge_en_store);
+static DEVICE_ATTR(dpr_en, 0664, dpr_en_show, dpr_en_store);
+static DEVICE_ATTR_RO(fan_duty);
+#endif
+
 static void max31760_set_cur_state_common(struct max31760_data *pdata,
 				unsigned long state)
 {
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	if (state > FAN_SPEED_LEVEL7)
+		state = FAN_SPEED_LEVEL7;
+#else
 	if (state > FAN_SPEED_LEVEL4)
 		state = FAN_SPEED_LEVEL4;
+#endif
 	if (state < FAN_SPEED_LEVEL0)
 		state = FAN_SPEED_LEVEL0;
 
@@ -82,7 +285,11 @@ static void max31760_set_cur_state_common(struct max31760_data *pdata,
 static int max31760_get_max_state(struct thermal_cooling_device *cdev,
 				unsigned long *state)
 {
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	*state = FAN_SPEED_LEVEL7;
+#else
 	*state = FAN_SPEED_LEVEL4;
+#endif
 	return 0;
 }
 
@@ -159,6 +366,37 @@ static int max31760_parse_dt(struct max31760_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	pdata->usb_charge_en_gpio = of_get_named_gpio(node,
+			"maxim,usb_charge_contrl", 0);
+	if (!gpio_is_valid(pdata->usb_charge_en_gpio)) {
+		dev_err(pdata->dev, "usb_charge_en_gpio not specified\n");
+		return -EINVAL;
+	}
+
+	ret = gpio_request(pdata->usb_charge_en_gpio, "usb_charge_en_gpio");
+	if (ret) {
+		pr_err("usb_charge_en_gpio request failed, ret:%d\n", ret);
+		return -EINVAL;
+	}
+
+	gpio_direction_output(pdata->usb_charge_en_gpio, 0);
+
+	pdata->dpr_en_gpio = of_get_named_gpio(node, "maxim,dpr_contrl", 0);
+	if (!gpio_is_valid(pdata->dpr_en_gpio)) {
+		dev_err(pdata->dev, "dpr_en_gpio not specified\n");
+		return -EINVAL;
+	}
+
+	ret = gpio_request(pdata->dpr_en_gpio, "dpr_en_gpio");
+	if (ret) {
+		pr_err("dpr_en_gpio request failed, ret:%d\n", ret);
+		return -EINVAL;
+	}
+
+	gpio_direction_output(pdata->dpr_en_gpio, 1);
+
+#else
 	pdata->max31760_en_gpio = of_get_named_gpio(node, "maxim,fan_en_gpio", 0);
 	if (!gpio_is_valid(pdata->max31760_en_gpio)) {
 		dev_err(pdata->dev, "max31760 enable gpio not specified\n");
@@ -177,6 +415,7 @@ static int max31760_parse_dt(struct max31760_data *pdata)
 
 error:
 	gpio_free(pdata->max31760_en_gpio);
+#endif
 	return ret;
 }
 
@@ -220,6 +459,26 @@ static int max31760_enable_vregs(struct max31760_data *pdata)
 		return -EINVAL;
 	}
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	pdata->vdda_reg = devm_regulator_get(pdata->dev, "maxim,vdda");
+	if (IS_ERR(pdata->vdda_reg)) {
+		ret = PTR_ERR(pdata->vdda_reg);
+		dev_err(pdata->dev, "couldn't get vdda_reg regulator, ret:%d\n", ret);
+		pdata->vdda_reg = NULL;
+		return ret;
+	}
+
+	regulator_set_voltage(pdata->vdda_reg, VDD_MIN_FAN, VDD_MAX_FAN);
+	regulator_set_load(pdata->vdda_reg, VDD_LOAD_UA);
+	ret = regulator_enable(pdata->vdda_reg);
+	if (ret < 0) {
+		dev_err(pdata->dev, "vdda_reg regulator failed, ret:%d\n", ret);
+		regulator_set_voltage(pdata->vdda_reg, 0, VDD_MAX_FAN);
+		regulator_set_load(pdata->vdda_reg, 0);
+		return -EINVAL;
+	}
+#endif
+
 	return 0;
 }
 
@@ -233,8 +492,12 @@ static int max31760_remove(struct i2c_client *client)
 	thermal_cooling_device_unregister(pdata->cdev);
 	regulator_disable(pdata->vdd_reg);
 	regulator_disable(pdata->vcca_reg);
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	regulator_disable(pdata->vdda_reg);
+#else
 	max31760_enable_gpio(pdata, 0);
 	gpio_free(pdata->max31760_en_gpio);
+#endif
 
 	return 0;
 }
@@ -283,14 +546,35 @@ static int max31760_probe(struct i2c_client *client, const struct i2c_device_id 
 		goto fail_register_cdev;
 	}
 
+#ifdef CONFIG_MACH_RAZER_NICOLE
+	Max31760_pdata = pdata;
+	if (sysfs_create_file(&(pdata->cdev->device.kobj), &dev_attr_fan_speed.attr)) {
+		dev_err(pdata->dev, "failed to create note fan_speed, ret:%d\n", ret);
+	}
+
+	if (sysfs_create_file(&(pdata->cdev->device.kobj), &dev_attr_fan_duty.attr)) {
+		dev_err(pdata->dev, "failed to create note fan_duty, ret:%d\n", ret);
+	}
+
+	if (sysfs_create_file(&(pdata->cdev->device.kobj), &dev_attr_usb_charge_en.attr)) {
+		dev_err(pdata->dev, "failed to create note usb_charge_en, ret:%d\n", ret);
+	}
+
+	if (sysfs_create_file(&(pdata->cdev->device.kobj), &dev_attr_dpr_en.attr)) {
+		dev_err(pdata->dev, "failed to create note dpr_en, ret:%d\n", ret);
+	}
+#endif
+
 	return ret;
 
 fail_register_cdev:
 	max31760_remove(client);
 	return ret;
 fail_enable_vregs:
+#ifndef CONFIG_MACH_RAZER_NICOLE
 	max31760_enable_gpio(pdata, 0);
 	gpio_free(pdata->max31760_en_gpio);
+#endif
 fail_parse_dt:
 	i2c_set_clientdata(client, NULL);
 	dev_set_drvdata(&client->dev, NULL);
@@ -310,8 +594,15 @@ static int max31760_suspend(struct device *dev)
 	if (pdata) {
 		atomic_set(&pdata->in_suspend, 1);
 		mutex_lock(&pdata->update_lock);
+#ifdef CONFIG_MACH_RAZER_NICOLE
+		pdata->cur_state = FAN_SPEED_LEVEL0;
+#endif
 		max31760_speed_control(pdata, FAN_SPEED_LEVEL0);
+#ifndef CONFIG_MACH_RAZER_NICOLE
 		max31760_enable_gpio(pdata, 0);
+#else
+		regulator_disable(pdata->vdda_reg);
+#endif
 		regulator_disable(pdata->vdd_reg);
 		mutex_unlock(&pdata->update_lock);
 	}
@@ -328,7 +619,13 @@ static int max31760_resume(struct device *dev)
 	if (pdata) {
 		atomic_set(&pdata->in_suspend, 0);
 		mutex_lock(&pdata->update_lock);
+#ifndef CONFIG_MACH_RAZER_NICOLE
 		max31760_enable_gpio(pdata, 1);
+#else
+		ret = regulator_enable(pdata->vdda_reg);
+		if (ret < 0)
+			dev_err(pdata->dev, "vdda_reg regulator failed, ret:%d\n", ret);
+#endif
 
 		ret = regulator_enable(pdata->vdd_reg);
 		if (ret < 0)
